eb1b4f3b279e04ca7454ca3d53b366da793d0079
ishimweghislain <ishimweghislain82@gmail.com>
2026-03-09 12:40:06 +0200

    Edited irregular loans also

--- app.heavenlyharvest-finance.com/modules/irregularloans.php
+++ app.heavenlyharvest-finance.com/modules/irregularloans.php
@@ -1164,7 +1164,7 @@ function createIrregularTransactionRecord($conn, $loan_id, $loan_number, $type, 
                         </div>
                         <div class="col-md-4">
                             <div class="mb-3">
-                                <label for="collateral_value" class="form-label">Collateral Value</label>
+                                <label for="collateral_value" class="form-label">Market Value</label>
                                 <input type="text" class="form-control money-input" id="collateral_value"
                                        name="collateral_value"
                                        value="<?php echo isset($_POST['collateral_value']) ? formatMoney(parseMoney($_POST['collateral_value'])) : '0'; ?>">
@@ -1172,7 +1172,7 @@ function createIrregularTransactionRecord($conn, $loan_id, $loan_number, $type, 
                         </div>
                         <div class="col-md-4">
                             <div class="mb-3">
-                                <label for="collateral_net_value" class="form-label">Net Value</label>
+                                <label for="collateral_net_value" class="form-label">Cost Sale Value</label>
                                 <input type="text" class="form-control money-input" id="collateral_net_value"
                                        name="collateral_net_value"
                                        value="<?php echo isset($_POST['collateral_net_value']) ? formatMoney(parseMoney($_POST['collateral_net_value'])) : '0'; ?>">
